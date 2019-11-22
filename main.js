dcea328717522ef8ea577d25786c3930ba1c0cbf
Sujit Das <56910023+Sujit-Das-Creator@users.noreply.github.com>
2019-11-22 22:04:10 +0530

    POrtfolio

---
+++ main.js
@@ -0,0 +1,6 @@
+//margin full body is working
+// let acBody = document.getElementById('acBody');
+// acBody.style.padding = '0px';
+// acBody.style.margin = '0px';
+// acBody.style.width='1200px';
+// acBody.style.backgroundColor='#fff';
